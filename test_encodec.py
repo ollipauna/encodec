59130abedd5f6587ea4bb7866496ede5e713dc45
Olli Pauna <olli.p.pauna@protonmail.com>
2026-02-19 15:05:10 +0200

    test multiple reconstructions

--- test_encodec.py
+++ test_encodec.py
@@ -7,17 +7,19 @@ def test_encodec():
     model = EncodecModel.encodec_model_24khz()
 
     # Create a random audio signal
-    x = torch.randn(1, 1, 16000, requires_grad=True)
+    x = torch.randn(2, 1, 16000, requires_grad=True)
 
     model.eval()
     x_org_quantized = model._original_forward(x)
 
     model.train()
-    x_quantized = model(x)
+    for _ in range(10):
+    
+        x_quantized = model(x)
 
-    # Check that the audio reconstructions match
-    assert torch.allclose(x_quantized, x_org_quantized, atol=1e-3)
-    assert x_quantized.size() == x.size()
+        # Check that the audio reconstructions match
+        assert torch.allclose(x_quantized, x_org_quantized, atol=1e-3)
+        assert x_quantized.size() == x.size()
 
     loss = torch.pow(x_quantized, 2).sum()
     loss.backward()
